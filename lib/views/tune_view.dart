1ab744174e5bbb0508d8e8d54f8fb37b6020607c
Mohamed Essam <77198018+MohamedEssam9009@users.noreply.github.com>
2024-04-13 16:15:23 +0200

    tune item

--- lib/views/tune_view.dart
+++ lib/views/tune_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tune_player_app/widgets/tune_item.dart';
 
 class TuneView extends StatelessWidget {
   const TuneView({super.key});
@@ -11,6 +12,31 @@ class TuneView extends StatelessWidget {
         elevation: 0.0,
         backgroundColor: const Color(0xFF253238),
         title: const Text('Flutter Tune'),
+      ),
+      body: const Column(
+        children: [
+          TuneItem(
+            color: Colors.red,
+          ),
+          TuneItem(
+            color: Colors.orange,
+          ),
+          TuneItem(
+            color: Colors.blue,
+          ),
+          TuneItem(
+            color: Colors.green,
+          ),
+          TuneItem(
+            color: Colors.black,
+          ),
+          TuneItem(
+            color: Colors.yellow,
+          ),
+          TuneItem(
+            color: Colors.red,
+          ),
+        ],
       ),
     );
   }
