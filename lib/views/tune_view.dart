b288d92e6b530321aeadfcfec92a9f57312b308f
Mohamed Essam <77198018+MohamedEssam9009@users.noreply.github.com>
2024-04-13 16:37:42 +0200

    Display tune items

--- lib/views/tune_view.dart
+++ lib/views/tune_view.dart
@@ -4,6 +4,16 @@ import 'package:tune_player_app/widgets/tune_item.dart';
 class TuneView extends StatelessWidget {
   const TuneView({super.key});
 
+  final List<Color> tuneColors = const [
+    Color(0xffF44336),
+    Color(0xffF89800),
+    Color(0xffFEEB3B),
+    Color(0xff4CAF50),
+    Color(0xff2F9688),
+    Color(0xff2896F3),
+    Color(0xff9C27B0),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,31 +23,15 @@ class TuneView extends StatelessWidget {
         backgroundColor: const Color(0xFF253238),
         title: const Text('Flutter Tune'),
       ),
-      body: const Column(
-        children: [
-          TuneItem(
-            color: Colors.red,
-          ),
-          TuneItem(
-            color: Colors.orange,
-          ),
-          TuneItem(
-            color: Colors.yellow,
-          ),
-          TuneItem(
-            color: Colors.green,
-          ),
-          TuneItem(
-            color: Colors.greenAccent,
-          ),
-          TuneItem(
-            color: Colors.blue,
-          ),
-          TuneItem(
-            color: Colors.purple,
-          ),
-        ],
-      ),
+      body: Column(children: getTuneItems()),
     );
+  }
+
+  List<TuneItem> getTuneItems() {
+    List<TuneItem> items = [];
+    for (Color color in tuneColors) {
+      items.add(TuneItem(color: color));
+    }
+    return items;
   }
 }
